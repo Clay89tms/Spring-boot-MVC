b798e564498ecfcc65d0f46abd9b2601b60360ee
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-03-23 11:37:29 +0300

    L44 Spring-boot-MVC thymeleaf PROJECT CW init com1

--- L44_SBMVC-thymeleaf/src/main/java/com/example/l44_sbmvcthymeleaf/domain/CarDto.java
+++ L44_SBMVC-thymeleaf/src/main/java/com/example/l44_sbmvcthymeleaf/domain/CarDto.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+@Table(name = "cars")
 public class CarDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String name;
     private Integer age;
