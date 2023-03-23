30f4ea42eca3b4777f63352f8c4eadeeda4b34a6
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-03-23 23:50:15 +0300

    L45 REST Classwork init com2

---
+++ L45_REST/src/main/java/com/example/l45_rest/L45RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.l45_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L45RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L45RestApplication.class, args);
+    }
+
+}
