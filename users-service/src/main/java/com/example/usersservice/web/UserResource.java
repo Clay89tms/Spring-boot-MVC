e1c955f25f42e0ad08eea300a0cc8e90995d4f2a
Clay89TMS <bboyClay89@gmail.com>
2023-03-30 16:17:44 +0300

    L46(47) REST Classwork add module users-service. init com7.2

---
+++ users-service/src/main/java/com/example/usersservice/web/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.usersservice.web;
+
+import com.example.usersservice.model.UserEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping("/{id}")
+    public UserEntity get(@PathVariable (name = "id") String login){
+
+    }
+}
