943d92f9e20f399dc8d30b80f625acfc9cdf31ca
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-04-03 23:32:53 +0300

    L46(47) REST Classwork add module users-service. init com7.5

---
+++ L46-47/store-service/src/main/java/com/example/storeservice/client/UserClient.java
@@ -0,0 +1,16 @@
+package com.example.storeservice.client;
+
+import com.example.storeservice.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "userClient",
+        url = "http://127.0.0.1:8081"
+)
+public interface UserClient {
+
+    @GetMapping("/users/{id}")
+    UserDto get(@PathVariable(name = "id") String login);
+}
