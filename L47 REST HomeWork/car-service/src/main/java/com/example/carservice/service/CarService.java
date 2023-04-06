e57a96fcb3617c278ebf2bebe660c27569260651
Clay89TMS <bboyClay89@gmail.com>
2023-04-06 17:43:28 +0300

    47 REST Homework Edit CarService commit 1.1

--- L47 REST HomeWork/car-service/src/main/java/com/example/carservice/service/CarService.java
+++ L47 REST HomeWork/car-service/src/main/java/com/example/carservice/service/CarService.java
@@ -5,6 +5,10 @@ import com.example.carservice.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RequiredArgsConstructor
 
 @Service
@@ -12,12 +16,30 @@ public class CarService {
 
     private final CarRepository repository;
 
-    public void createCar(CarEntity car) {
+    public void createCarInBD(CarEntity car) {
         repository.save(car);
     }
 
-    public CarEntity getCarInDB(CarEntity car) {
-        //+Errors
-        return repository.findByModel(car.getModel());
+    public CarEntity getCarInDB(UUID id) {
+        //add my error class
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("dont find a car"));
+    }
+
+    public List<CarEntity> getAllCarInBD() {
+        return repository.findAll();
+    }
+
+    public CarEntity updateCarInBD(CarEntity car) {
+        //add my error class/ del null
+        if(repository.existsById(car.getId())) {
+            return repository.save(car);
+        }
+        return null;
+    }
+
+
+    public void deleteCarById(UUID id) {
+        var carByIdFromBD = repository.findById(id);
+        repository.deleteById(id);
     }
 }
--- L47 REST HomeWork/car-service/src/main/java/com/example/carservice/web/CarResource.java
+++ L47 REST HomeWork/car-service/src/main/java/com/example/carservice/web/CarResource.java
@@ -3,11 +3,16 @@ package com.example.carservice.web;
 import com.example.carservice.model.CarEntity;
 import com.example.carservice.service.CarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 
@@ -18,12 +23,27 @@ public class CarResource {
     private final CarService service;
 
     @PostMapping
-    public void save(@RequestBody CarEntity car){
-        service.createCar(car);
+    public void saveCar(@RequestBody CarEntity car){
+        service.createCarInBD(car);
     }
 
     @GetMapping
-    public CarEntity get(@RequestBody CarEntity car){
-        return service.getCarInDB(car);
+    public CarEntity getCarById(@RequestBody UUID id){
+        return service.getCarInDB(id);
+    }
+
+    @GetMapping("/all")
+    public List<CarEntity> getAllCar(){
+        return service.getAllCarInBD();
+    }
+
+    @PutMapping
+    public CarEntity update(@RequestBody CarEntity car){
+        return service.updateCarInBD(car);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody UUID id){
+        service.deleteCarById(id);
     }
 }
