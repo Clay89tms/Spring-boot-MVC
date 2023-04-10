410504251b8969fa68cd95dade8e5b1f5eaed994
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-04-10 22:50:23 +0300

    48 Security Classwork Login security commit 1.3

--- tms-security-spring/src/main/java/com/example/security/model/User.java
+++ tms-security-spring/src/main/java/com/example/security/model/User.java
@@ -7,25 +7,33 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name = "pesons")
 public class User implements UserDetails {
 
+    @Id
+    private UUID id;
     private String username;
     private String password;
-    private List<String> auths;
+
+    private String auths;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (auths != null && !auths.isEmpty()) {
-            return auths.stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
+        if (auths != null) {
+            return List.of(new SimpleGrantedAuthority(auths));
         } else {
             return null;
         }
