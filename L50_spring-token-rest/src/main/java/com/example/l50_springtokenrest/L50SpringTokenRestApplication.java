2cd1bf7d5b415c555829327b2f274f977535f7e4
Clay89TMS <bboyClay89@gmail.com>
2023-04-19 14:47:28 +0300

    50 Security JWT Classwork Login security from REST commit 1.0

---
+++ L50_spring-token-rest/src/main/java/com/example/l50_springtokenrest/L50SpringTokenRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.l50_springtokenrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L50SpringTokenRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L50SpringTokenRestApplication.class, args);
+    }
+
+}
---
+++ L50_spring-token-rest/src/test/java/com/example/l50_springtokenrest/L50SpringTokenRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l50_springtokenrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L50SpringTokenRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
